b8bf97f60013be99b38d71a7d1a58c6e8191d7f5
Your Name <you@example.com>
2023-03-12 23:09:29 -0600

    api và giao diện khách hàng

--- QuanLyKhachSan/Controllers/KhachHangsController.cs
+++ QuanLyKhachSan/Controllers/KhachHangsController.cs
@@ -53,7 +53,7 @@ namespace QuanLyKhachSan.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("MaKhachHang,HoTen,NgaySinh,DiaChi,Sdt,Email")] KhachHang khachHang)
+        public async Task<IActionResult> Create([Bind("HoTen,NgaySinh,DiaChi,Sdt,Email")] KhachHang khachHang)
         {
             if (ModelState.IsValid)
             {
